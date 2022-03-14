2bf343dbac6bc817e6889ce534c9dccbdafbd9eb
Shehan Bartholomeusz <it20274702@my.sliit.lk>
2022-03-14 11:19:32 +0530

    challenge 1 implementaion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from 'react';
+import ComponentA from './ComponentA';
+import ComponentB from './ComponentB';
+
+//functional component implementation
+// function App() {
+//   const [count, setCount] = useState(10);
+//   return(
+//     <><h1>counter value = {count}</h1>
+//     <ComponentA setCount={setCount} count={count} />
+//     <ComponentB setCount={setCount} count={count} />
+//     </>
+//   )
+// }
+
+
+//class component implementation
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 10
+    }
+    this.handleCount = this.handleCount.bind(this);
+  }
+
+  handleCount(event){
+    const id = event.target.id;
+    switch(id) {
+      case 'increment':
+        this.setState({ count: this.state.count + 1 });
+        break;
+      case 'decrement':
+        this.setState({ count: this.state.count - 1 });
+        break;
+      default:
+        break;
+    }
+  }
+
+  render() {
+    return (
+      <><h1>counter value = {this.state.count}</h1>
+      <ComponentA handleCount={this.handleCount} />
+      <ComponentB handleCount={this.handleCount} />
+      </>
+    )
+  }
 }
 
 export default App;
